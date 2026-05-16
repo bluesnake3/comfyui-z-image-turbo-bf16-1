41148cbd5215affc27ca2b9c26c8d518a9e9a2e9
bluesnake3 <115251329+bluesnake3@users.noreply.github.com>
2026-05-16 20:23:20 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Activate persistent venv"
+
+source /runpod-volume/venv/bin/activate
+
+echo "Link custom nodes"
+
 mkdir -p /comfyui/custom_nodes
 
 for dir in /runpod-volume/custom_nodes/*; do
@@ -7,5 +13,7 @@ for dir in /runpod-volume/custom_nodes/*; do
 
     ln -sf "$dir" "/comfyui/custom_nodes/$name"
 done
+
+echo "Start ComfyUI worker"
 
 python /start.py
